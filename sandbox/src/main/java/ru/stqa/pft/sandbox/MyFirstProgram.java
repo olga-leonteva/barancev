1fe51c62276eba44f2d0040cdab19ac91f0a95bc
Olga Uvarova <leonteva.h@gmail.com>
2018-07-09 13:40:18 +0300

    Довавлены математические примеры и склеевание строк

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,5 +3,22 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+
+        System.out.println(1 / 2); // результат будет 0, тк идет округление до ближайшего целого
+        System.out.println(1.0 / 2); // результат 0,5 , тк указано число с плавающей точкой
+        System.out.println(1 / 2.0);
+        System.out.println(2.0 / 2);
+
+        System.out.println("2" + "2"); // склеивание строк - конкатенация
+        System.out.println("2" + 2); //число преобразуется в строку и значение будет 22
+
+        System.out.println("2 + 2 = " + (2 + 2));
+
+
     }
 }
