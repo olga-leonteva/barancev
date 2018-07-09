ce29b13f4abfeaf767decfebf4221868c07249dd
Olga Uvarova <leonteva.h@gmail.com>
2018-07-09 13:57:51 +0300

    Создан новый класс, где введены первые переменные

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram2.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram2 {
+  public static void main (String[] args){
+    System.out.println("Hello, " + "world!");
+
+    int l = 6;
+    int s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+  }
+}
