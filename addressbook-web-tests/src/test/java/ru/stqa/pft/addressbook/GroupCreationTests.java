84b45ccec39473b30cb27f037872c40bbbed4801
Olga Uvarova <Olga.Uvarova@emc.com>
2018-08-02 15:52:53 +0300

    добавлен тест для создания групп

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -0,0 +1,123 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+public class GroupCreationTests {
+//  private WebDriver driver;
+public static WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+//  public void setUp() throws Exception {
+//   // WebDriverWait wait = new WebDriverWait(driver,10);
+//    driver = new FirefoxDriver();
+////    baseUrl = "https://www.katalon.com/";
+//    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+//  }
+
+  public static void setUpDriver() throws Exception {
+    driver = setUpChromeDriver();
+
+  }
+
+  private static WebDriver setUpChromeDriver() throws Exception {
+    System.out.println("= = = = = = = = = = = = = = =");
+    System.out.println("Setting up Chrome Web Driver");
+
+    String chromeDriver = System.getenv().getOrDefault(
+            "WEBDRIVER_CHROME_DRIVER",
+            "C://Tools//Webdrivers//chromedriver.exe"
+    );
+
+    System.out.println("WEBDRIVER_CHROME_DRIVER = " + chromeDriver);
+
+    System.setProperty("webdriver.chrome.driver", chromeDriver);
+    WebDriver driver = new ChromeDriver();
+
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+
+    System.out.println("= = = = = = = = = = = = = = =");
+
+    return driver;
+  }
+
+  @Test
+  public void testUntitledTestCase() throws Exception {
+    driver.get("http://localhost/addressbook/");
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.xpath("//input[@value='Login']")).click();
+    driver.findElement(By.linkText("groups")).click();
+    driver.findElement(By.name("new")).click();
+    driver.findElement(By.name("group_name")).click();
+    driver.findElement(By.name("group_name")).clear();
+    driver.findElement(By.name("group_name")).sendKeys("test");
+    driver.findElement(By.name("group_header")).click();
+    driver.findElement(By.name("group_header")).clear();
+    driver.findElement(By.name("group_header")).sendKeys("test1");
+    driver.findElement(By.name("group_footer")).click();
+    driver.findElement(By.name("group_footer")).clear();
+    driver.findElement(By.name("group_footer")).sendKeys("test2");
+    driver.findElement(By.name("submit")).click();
+    driver.findElement(By.linkText("group page")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
